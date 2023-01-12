49373caa6e27d4d843389b6185e404bf905ad137
Dre <114764373+DreaUltimate@users.noreply.github.com>
2023-01-12 11:00:31 +0800

    Update the server URL

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch Rea's Response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://rea-ai.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
